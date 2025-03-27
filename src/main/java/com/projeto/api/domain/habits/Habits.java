272e58728ddd8c0fc1e3b3d1ea2b69c22cfccce5
Felipe <coutodev@icloud.com>
2025-03-27 18:02:45 -0300

    feat: adicionar classe Habits para gerenciamento de hábitos
    Adicionada a classe `Habits` para representar hábitos dentro do sistema. Esta classe inclui campos para detalhes do hábito, como nome do hábito, frequência, data de criação e uma referência ao usuário associado. Utiliza anotações do JPA para mapeamento de entidade e relacionamento com a tabela de banco de dados.

--- src/main/java/com/projeto/api/domain/habits/Habits.java
+++ src/main/java/com/projeto/api/domain/habits/Habits.java
@@ -1,23 +1,28 @@
 package com.projeto.api.domain.habits;
 
-import com.projeto.api.domain.user.Users;
+import com.projeto.api.domain.user.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Table(name = "habits")
+@Entity(name = "habits")
 @Getter
-@Setter
-@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class Habits {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false) // Define a chave estrangeira
-    private Users users;
-    private String nome;
+    private User user;
+    private String nome_habito;
     private String frequencia;
     private LocalDate criado_em;
 }

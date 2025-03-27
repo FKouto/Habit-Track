35ee837703c80dc231d69951a4c36633958d5457
Felipe <coutodev@icloud.com>
2025-03-27 18:16:55 -0300

    refactor: melhorar o código da classe TokenService
    Melhorado o código da classe `TokenService` para otimizar a geração e validação de tokens JWT. A classe agora utiliza o algoritmo HMAC256 com uma chave secreta configurada via propriedades.

--- src/main/java/com/projeto/api/infra/security/TokenService.java
+++ src/main/java/com/projeto/api/infra/security/TokenService.java
@@ -2,28 +2,50 @@ package com.projeto.api.infra.security;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import com.projeto.api.domain.user.Users;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.projeto.api.domain.user.User;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.time.Instant;
-import java.time.temporal.ChronoUnit;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
-@Component
-public class JwtUtil {
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
 
-    private final Algorithm algorithm;
-
-    public JwtUtil(@Value("${jwt.secret}") String secretKey) {
-        this.algorithm = Algorithm.HMAC256(secretKey);
+    public String generateToken(User user) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("auth")
+                    .withSubject(user.getEmail())
+                    .withExpiresAt(genExpirationDate())
+                    .sign(algorithm);
+            return token;
+        } catch (JWTCreationException exception) {
+            throw new RuntimeException("Error while generating token", exception);
+        }
     }
 
-    public String generateToken(Users user) {
-        return JWT.create()
-                .withSubject(user.getEmail())
-                .withClaim("userId", user.getId())
-                .withIssuedAt(Instant.now())
-                .withExpiresAt(Instant.now().plus(1, ChronoUnit.DAYS))
-                .sign(algorithm);
+
+    public String validateToken(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("auth")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException exception) {
+            return "";
+        }
+    }
+
+    private Instant genExpirationDate() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
     }
 }

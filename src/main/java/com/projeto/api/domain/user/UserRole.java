a7ef593c313900d0c7a939b121dcf5a5ee42ca63
Felipe <coutodev@icloud.com>
2025-03-27 17:59:06 -0300

    feat: adicionar enum UserRole para gerenciamento de papéis de usuário :green_heart:
    Adicionada a enumeração `UserRole` para definir e gerenciar papéis de usuário dentro do sistema. Esta enumeração atualmente inclui um único papel, "USER", e fornece um método para recuperar o papel como uma string.

---
+++ src/main/java/com/projeto/api/domain/user/UserRole.java
@@ -0,0 +1,15 @@
+package com.projeto.api.domain.user;
+
+public enum UserRole {
+    USER("user");
+
+    private final String role;
+
+    UserRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() {
+        return role;
+    }
+}

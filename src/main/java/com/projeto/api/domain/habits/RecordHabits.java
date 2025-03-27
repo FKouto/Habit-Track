0d5c6dea26b7def179f7961d0505ed2bb3f10999
Felipe <coutodev@icloud.com>
2025-03-27 18:05:07 -0300

    feat: adicionar classe HabitsRecord para registro de hábitos
    Adicionada a classe `HabitsRecord` para representar o registro de hábitos dentro do sistema. Esta classe inclui campos para detalhes do registro, como usuário, hábito, data e período. Utiliza anotações do JPA para mapeamento de entidade e relacionamento com a tabela de banco de dados.

--- src/main/java/com/projeto/api/domain/habits/RecordHabits.java
+++ src/main/java/com/projeto/api/domain/habits/RecordHabits.java
@@ -1,28 +1,31 @@
 package com.projeto.api.domain.habits;
 
-import com.projeto.api.domain.user.Users;
-import com.projeto.api.enums.Period;
+import com.projeto.api.domain.user.User;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
+@Table(name = "habits_record")
+@Entity(name = "habits_record")
 @Getter
-@Setter
-@Entity
-public class RecordHabits {
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class HabitsRecord {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false) // Define a chave estrangeira
-    private Users users;
+    private User user;
     @ManyToOne
     @JoinColumn(name = "habits_id", nullable = false) // Define a chave estrangeira
     private Habits habits;
     private LocalDate data;
     @Enumerated(EnumType.STRING)
     private Period period;
-    private boolean realizado;
 }

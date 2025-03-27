c88c6894c6a967b90fca92ee090d66655c6aedf2
Felipe <coutodev@icloud.com>
2025-03-27 18:09:23 -0300

    feat: adicionar repositório HabitsRepository para gerenciamento de hábitos
    Adicionado o repositório `HabitsRepository` para gerenciar operações de persistência de hábitos. Este repositório estende `JpaRepository` e permite operações CRUD na entidade `Habits`.

--- src/main/java/com/projeto/api/repositories/HabitsRepository.java
+++ src/main/java/com/projeto/api/repositories/HabitsRepository.java
@@ -1,6 +1,6 @@
-package com.projeto.api.repository;
+package com.projeto.api.repositories;
 
-import com.projeto.api.model.Habits;
+import com.projeto.api.domain.habits.Habits;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface HabitsRepository extends JpaRepository<Habits, Long> {
